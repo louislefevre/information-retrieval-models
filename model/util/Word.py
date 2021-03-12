dc813e8131f5ccc8e25587217c5514e0e8167ac5
Louis Lefevre <louislefev@gmail.com>
2021-03-12 23:04:03 +0000

    Set and sort words list

--- model/util/Word.py
+++ model/util/Word.py
@@ -17,8 +17,10 @@ class Word:
         return self._freq
 
     @staticmethod
-    def convert(file_name: str) -> list[object]:
+    def convert(file_name: str) -> list:
         text = read(file_name)
         tokens = clean(text)
         dist = FreqDist(tokens)
-        return [Word(token, dist.freq(token)) for token in tokens]
+        words = [Word(token, dist.freq(token)) for token in set(tokens)]
+        words.sort(key=lambda word: word.freq, reverse=True)
+        return words
