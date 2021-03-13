e7f35293afa2d7dc382ffa244245c5a902f7327f
Louis Lefevre <louislefev@gmail.com>
2021-03-13 00:00:30 +0000

    Remove CSV read function

--- model/util/FileReader.py
+++ model/util/FileReader.py
@@ -5,7 +5,7 @@ from typing import Union, IO
 def read(file_name: str) -> Union[list, str]:
     file = open(file_name)
     if file_name.endswith('.tsv'):
-        return _read_csv(file)
+        return _read_tsv(file)
     elif file_name.endswith('.txt'):
         return _read_txt(file)
     else:
@@ -13,10 +13,6 @@ def read(file_name: str) -> Union[list, str]:
 
 
 def _read_tsv(file: IO) -> list:
-    return _read_csv(file)
-
-
-def _read_csv(file: IO) -> list:
     return list(csv.reader(file, delimiter="\t"))
 
 
