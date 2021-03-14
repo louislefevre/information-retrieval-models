9ecfa018331e42dda43385a9ea60da4ece089883
Louis Lefevre <louislefev@gmail.com>
2021-03-14 18:10:15 +0000

    Add numbers conversion
    Text preprocessing now converts numbers to their textual format.

--- model/util/TextProcessor.py
+++ model/util/TextProcessor.py
@@ -1,13 +1,16 @@
+# import nltk
 # nltk.download('punkt')
 # nltk.download('stopwords')
 
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from num2words import num2words
 
 
 def clean(text: str) -> list[str]:
     tokens = tokenize(text)
+    tokens = convert_numbers(tokens)
     tokens = normalise(tokens)
     tokens = remove_stopwords(tokens)
     tokens = stem(tokens)
@@ -16,6 +19,10 @@ def clean(text: str) -> list[str]:
 
 def tokenize(text: str) -> list[str]:
     return word_tokenize(text)
+
+
+def convert_numbers(tokens: list[str]) -> list[str]:
+    return [num2words(word) if word.isnumeric() else word for word in tokens]
 
 
 def normalise(tokens: list[str]) -> list[str]:
