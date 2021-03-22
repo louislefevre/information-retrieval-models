79ef811760372c8bc19bcc86753b61c9deda07b6
Louis Lefevre <louislefev@gmail.com>
2021-03-22 22:17:47 +0000

    Revert to previous BM25 formula

--- retrieval/models/BM25.py
+++ retrieval/models/BM25.py
@@ -12,25 +12,19 @@ class BM25(Model):
 
     def _score_passage(self, pid: int, word: str) -> float:
         # Constants
-        r = 0
-        R = 0.0
         k1 = 1.2
-        k2 = 100.0
         b = 0.75
-        qf = 1
 
         # Parameters
         inv_index = self._index[word]
-        n = inv_index.doc_freq
         N = self._collection_length
+        n = inv_index.doc_freq
+        f = inv_index.get_posting(pid).freq
         dl = float(len(self._collection[pid]))
         avg_dl = float(self._avg_length)
-        f = inv_index.get_posting(pid).freq
-        K = k1 * ((1 - b) + b * (dl / avg_dl))
 
         # Formulas
-        expr_1 = log(((r + 0.5) / (R - r + 0.5)) /
-                     ((n - r + 0.5) / (N - n - R + r + 0.5)))
-        expr_2 = ((k1 + 1) * f) / (K + f)
-        expr_3 = ((k2 + 1) * qf) / (k2 + qf)
-        return expr_1 * expr_2 * expr_3
+        K = k1 * ((1 - b) + b * (dl / avg_dl))
+        expr_1 = log(((N - n + 0.5) / (n + 0.5)) + 1)
+        expr_2 = ((f * (k1 + 1)) / (f + K))
+        return expr_1 * expr_2
