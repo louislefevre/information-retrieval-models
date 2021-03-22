77018bb88abac2e22fcdd11c3b1b76bfb84a0cd8
Louis Lefevre <louislefev@gmail.com>
2021-03-22 02:11:28 +0000

    Add InvertedList
    The InvertedIndex now stores InvertedLists as its values.

--- retrieval/InvertedIndex.py
+++ retrieval/InvertedIndex.py
@@ -6,51 +6,36 @@ from math import log10
 class InvertedIndex:
     def __init__(self, collection: dict[int, str]):
         self._collection = clean_collection(collection)
-        self._index: dict[str, list['Posting']] = dict()
+        self._index: dict[str, InvertedList] = dict()
 
-    def index_collection(self):
+    def parse(self):
         for pid, terms in self._collection.items():
             self._index_passage(pid, terms)
-
-        for term, postings in self._index.items():
-            for posting in postings:
-                posting.tfidf = self._tf_idf(term, posting)
+        for term, inverted_list in self._index.items():
+            self._tfidf_passage(term, inverted_list)
         return self._index
 
     def _index_passage(self, pid: int, terms: list[str]):
-        seen_terms = dict()
         for pos, term in enumerate(terms):
-            if term in seen_terms:
-                term_frequency = seen_terms[term].freq
-                positions = seen_terms[term].positions
-            else:
-                term_frequency = 0
-                positions = []
-            seen_terms[term] = Posting(pid, term_frequency+1, positions+[pos])
-
-        for term, posting in seen_terms.items():
             if term not in self._index:
-                self._index[term] = [posting]
-            else:
-                self._index[term] += [posting]
+                self._index[term] = InvertedList()
+            self._index[term].add_posting(pid, pos)
 
-    def _tf_idf(self, term: str, posting: 'Posting') -> float:
-        # Frequency of term in passage divided by number of words in the passage
-        tf = posting.freq / len(self._collection[posting.pointer])
-
-        # Number of occurrences of the term in all passages.
-        df = len(self._index[term])
-
-        # Log total passage count divided by df.
-        idf = log10(len(self._collection) / df)
-
-        return tf * idf
+    def _tfidf_passage(self, term: str, inv_index: 'InvertedList'):
+        for pointer, posting in inv_index.postings.items():
+            # Frequency of term in passage divided by number of words in the passage.
+            tf = posting.freq / len(self._collection[pointer])
+            # Number of occurrences of the term in all passages.
+            df = self._index[term].doc_freq
+            # Log total passage count divided by df.
+            idf = log10(len(self._collection) / df)
+            posting.tfidf = tf * idf
 
     def display(self):
-        for term, postings in self._index.items():
+        for term, inv_index in self._index.items():
             posts = ""
-            for post in postings:
-                posts += str([post.pointer, post.freq, post.tfidf, post.positions]) + " "
+            for pointer, posting in inv_index.postings.items():
+                posts += str(f'{pointer}: {[posting.freq, posting.positions, posting.tfidf]}, ')
             print(f"{term} : {posts}")
 
     @property
@@ -58,7 +43,7 @@ class InvertedIndex:
         return self._collection
 
     @property
-    def index(self) -> dict[str, list['Posting']]:
+    def index(self) -> dict[str, 'InvertedList']:
         return self._index
 
     @property
@@ -71,9 +56,42 @@ class InvertedIndex:
         return word_sum / self.collection_length
 
 
+class InvertedList:
+    def __init__(self):
+        self._postings: dict[int, 'Posting'] = {}
+        self._doc_freq: int = 0
+
+    def add_posting(self, pid: int, position: int):
+        if self.contains_posting(pid):
+            return self.update_posting(pid, position)
+        posting = Posting(1, [position])
+        self._postings[pid] = posting
+        self._doc_freq += 1
+
+    def update_posting(self, pid: int, position: int):
+        if not self.contains_posting(pid):
+            return self.add_posting(pid, position)
+        posting = self._postings[pid]
+        posting.freq += 1
+        posting.positions += [position]
+
+    def get_posting(self, pid: int) -> 'Posting':
+        return self._postings[pid]
+
+    def contains_posting(self, pid: int) -> bool:
+        return pid in self._postings
+
+    @property
+    def postings(self) -> dict[int, 'Posting']:
+        return self._postings
+
+    @property
+    def doc_freq(self) -> int:
+        return self._doc_freq
+
+
 @dataclass
 class Posting:
-    pointer: int
     freq: int
     positions: list[int]
     tfidf: float = 0.0
