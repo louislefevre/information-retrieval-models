2ca8719afda8d2f149714c233af5d83a5072ed74
Louis Lefevre <louislefev@gmail.com>
2021-03-25 04:13:40 +0000

    [WIP] VectorSpace model progress

--- retrieval/models/VectorSpace.py
+++ retrieval/models/VectorSpace.py
@@ -1,6 +1,7 @@
-import math
-from math import log10
+from collections import Counter
+from math import log
 
+import numpy as np
 from numpy import dot
 from numpy.linalg import norm
 
@@ -13,58 +14,57 @@ class VectorSpace(Model):
     def __init__(self, index: 'InvertedIndex'):
         super().__init__(index)
         self._collection_length = index.collection_length
-        self._vocab = index.vocab
+        self._vocab = sorted(index.vocab)
         self._vocab_count = index.vocab_count
 
     def _parse_query(self, query: str) -> dict[int, float]:
-        passage_vectors, query_vectors = self._vectorise(query)
+        query_words = [word for word in clean(query)]
+        passage_vectors = self._vectorise_passages(query_words)
+        query_vectors = self._vectorise_query(query_words)
+
         passage_scores = {}
-
-        for pid, vectors in passage_vectors.items():
-            passage_scores[pid] = self._cos_sim(query_vectors, vectors)
-
+        for pid, vector in passage_vectors.items():
+            passage_scores[pid] = self._similarity(query_vectors, vector)
         return passage_scores
 
-    def _vectorise(self, query: str) -> tuple[dict[int, list[float]], list[float]]:
-        query_keywords = [word for word in clean(query)]
-        passage_vectors = {}
-        query_vectors = [0.0] * self._vocab_count
+    def _vectorise_passages(self, query_words: list[str]) -> dict[int, np.ndarray]:
+        relevant_passages = set()
+        for word in query_words:
+            if word not in self._index:
+                continue
+            for pid in self._index[word].postings:
+                relevant_passages.add(pid)
 
-        for word in query_keywords:
+        passage_vectors = {}
+        for term, inv_list in self._index.items():
+            index = self._vocab.index(term)
+            for pid, posting in inv_list.postings.items():
+                if pid not in relevant_passages:
+                    continue
+                if pid not in passage_vectors:
+                    passage_vectors[pid] = np.zeros(self._vocab_count)
+                passage_vectors[pid][index] = posting.tfidf
+        return passage_vectors
+
+    def _vectorise_query(self, query_words: list[str]) -> np.ndarray:
+        query_vectors = np.zeros(self._vocab_count)
+        counter = Counter(query_words)
+        words_count = len(query_words)
+        for word in query_words:
             if word not in self._index:
                 continue
             index = self._vocab.index(word)
 
             # Query vectorisation
-            tf = query.count(word) / self._index[word].doc_freq
+            tf = counter[word] / words_count
             df = self._index[word].doc_freq
-            idf = log10(self._collection_length / df)
-            query_vectors[index] = tf * idf
+            idf = log(self._collection_length / df)
+            query_vectors[index] = (tf / df) * idf
 
-            # Passage vectorisation
-            for pid in self._index[word].postings.keys():
-                if pid not in passage_vectors:
-                    passage_vectors[pid] = [0.0] * self._vocab_count
-                tfidf = self._index[word].get_posting(pid).tfidf
-                passage_vectors[pid][index] = tfidf
-
-        return passage_vectors, query_vectors
+        return query_vectors
 
     @staticmethod
-    def _cos_sim(query_vectors: list[float], passage_vectors: list[float]) -> float:
+    def _similarity(query_vectors: np.ndarray, passage_vectors: np.ndarray) -> float:
         dot_product = dot(query_vectors, passage_vectors)
         norms = norm(query_vectors) * norm(passage_vectors)
         return dot_product / norms
-        # return spatial.distance.cosine(query_vectors, passage_vectors)
-
-    @staticmethod
-    def cosine_similarity(v1, v2):
-        "compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
-        sumxx, sumxy, sumyy = 0, 0, 0
-        for i in range(len(v1)):
-            x = v1[i]
-            y = v2[i]
-            sumxx += x * x
-            sumyy += y * y
-            sumxy += x * y
-        return sumxy / math.sqrt(sumxx * sumyy)
