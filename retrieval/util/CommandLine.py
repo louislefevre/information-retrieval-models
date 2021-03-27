e8566aae45739b4a29c166bbfdd0cf68cc612fee
Louis Lefevre <louislefev@gmail.com>
2021-03-27 05:53:50 +0000

    Fix command line parsing

--- retrieval/util/CommandLine.py
+++ retrieval/util/CommandLine.py
@@ -5,22 +5,21 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Information retrieval models.')
 
     # Dataset argument
-    parser.add_argument('dataset', dest='dataset',
-                        help='dataset for retrieving passages and queries')
+    parser.add_argument('dataset', help='dataset for retrieving passages and queries')
 
     # Models arguments
     models = parser.add_mutually_exclusive_group(required=True)
-    models.add_argument('--bm25', dest='bm25', help='BM25 model')
-    models.add_argument('--vector', dest='vector_space', help='Vector Space model')
-    models.add_argument('--query', dest='query_likelihood', help='Query Likelihood model')
+    models.add_argument('--bm25', action='store_true', help='BM25 model')
+    models.add_argument('--vector', action='store_true', help='Vector Space model')
+    models.add_argument('--query', action='store_true', help='Query Likelihood model')
 
     # Smoothing arguments
     smoothing = parser.add_mutually_exclusive_group(required=False)
-    smoothing.add_argument('--laplace', dest='laplace',
+    smoothing.add_argument('--laplace', action='store_true',
                            help='laplace smoothing for the Query Likelihood model')
-    smoothing.add_argument('--lidstone', dest='lidstone',
+    smoothing.add_argument('--lidstone', action='store_true',
                            help='lidstone smoothing for the Query Likelihood model')
-    smoothing.add_argument('--dirichlet', dest='dirichlet',
+    smoothing.add_argument('--dirichlet', action='store_true',
                            help='dirichlet smoothing for the Query Likelihood model')
 
     return parser.parse_args()
