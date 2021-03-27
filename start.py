9d435e8bfff0bab9ed969576e808d8fe2c73cfa6
Louis Lefevre <louislefev@gmail.com>
2021-03-27 18:02:11 +0000

    Add plot argument to CLI parser

--- start.py
+++ start.py
@@ -7,14 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description='Information retrieval models.')
     parser.add_argument('dataset', help='dataset for retrieving passages and queries')
     parser.add_argument('model', help='model for ranking passages against queries')
+    parser.add_argument('-p', '--plot', action='store_true', help='generate term frequency graph')
     parser.add_argument('-s', '--smoothing', help='smoothing for the Query Likelihood model')
 
     args = parser.parse_args()
-    model = args.model
-    smoothing = args.smoothing
-
     parser = DatasetParser(args.dataset)
-    results = parser.parse(model, smoothing=smoothing)
+    results = parser.parse(args.model, plot_freq=args.plot, smoothing=args.smoothing)
 
 
 if __name__ == '__main__':
